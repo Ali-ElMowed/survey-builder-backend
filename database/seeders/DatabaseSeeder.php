b6e6441a5e9bf25f55b7fa1169e8e490cbf0fe7b
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-17 11:01:38 +0300

    Added seeders for question types

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+            DB::table('question_types')->insert([
+                'type'=>'muliple choice',
+            ]);
+            DB::table('question_types')->insert([
+                'type'=>'check boxes',
+            ]);
+            DB::table('question_types')->insert([
+                'type'=>'drop down',
+            ]);
+            DB::table('question_types')->insert([
+                'type'=>'date',
+            ]);
+
+
     }
 }
